b4775d3b53bd2044aabb42f36d106b11390d7801
ilfei <nsteven@163.com>
2026-02-26 22:53:25 +0800

    新增 Orders 模块错误定义

---
+++ src/Modules/Orders/OrdersErrorCodes.cs
@@ -0,0 +1,8 @@
+﻿namespace Zss.BilliardHall.Modules.Orders;
+
+public static class OrdersErrorCodes
+{
+    public const string AlreadyPaid = "ORDERS_ALREADY_PAID";
+    public const string NotFound = "ORDERS_NOT_FOUND";
+}
+
---
+++ src/Modules/Orders/OrdersErrorModule.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Zss.BilliardHall.Platform.Errors;
+
+namespace Zss.BilliardHall.Modules.Orders;
+
+public sealed class OrdersErrorModule : IErrorModule
+{
+    public void Register()
+    {
+        ErrorRegistry.Register(new ErrorDescriptor(
+            OrdersErrorCodes.AlreadyPaid,
+            "订单已支付",
+            StatusCodes.Status409Conflict,
+            "https://api.zss.com/problems/orders/already-paid",
+            LogLevel.Information));
+
+        ErrorRegistry.Register(new ErrorDescriptor(
+            OrdersErrorCodes.NotFound,
+            "订单不存在",
+            StatusCodes.Status404NotFound,
+            "https://api.zss.com/problems/orders/not-found",
+            LogLevel.Debug));
+    }
+}
+
